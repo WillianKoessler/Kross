a8b42ba5dfd8226d7fc186a9c2c306aaa195b250
WillianKoessler <williankoessler@hotmail.com>
2021-12-13 05:46:41 -0300

    history assets updated

--- Editor/versions/assets/scripts/native/SceneCameraController.h
+++ Editor/versions/assets/scripts/native/SceneCameraController.h
@@ -3,23 +3,24 @@
 #include <glm/glm.hpp>
 #include "Kross/Scene/ScriptableEntity.h"
 #include "Kross/Core/Input.h"
+#include "Kross/Scene/Components.h"
 
 namespace Kross {
 	class CameraController : public ScriptableEntity
 	{
-		glm::vec4 vel;
+		glm::vec3 vel;
 	public:
-		void OnCreate() { vel = glm::vec4(0.0f); }
+		void OnCreate() { vel = glm::vec3(0.0f); }
 		void OnUpdate(double ts)
 		{
 			float speed = 8.0f * (float)ts;
-			glm::vec4 acc = glm::vec4(0.0f);
+			glm::vec3 acc = glm::vec3(0.0f);
 			if (Input::IsKeyHeld(Key::Up)) acc.y += speed;
 			if (Input::IsKeyHeld(Key::Down)) acc.y -= speed;
 			if (Input::IsKeyHeld(Key::Right)) acc.x += speed;
 			if (Input::IsKeyHeld(Key::Left)) acc.x -= speed;
 			vel = (vel * 0.95f) + acc;
-			Get<TransformComponent>().Transform[3] += vel;
+			Get<TransformComponent>()->Position += vel;
 		}
 	};
 }
